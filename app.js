aedc6c870adc99a333a8141cfcdb28045bc7ab3b
yozan21 <yojankaphle80@gmail.com>
2025-07-19 00:09:16 +0545

    Fixed rate-limiter bypass

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const bookingRouter = require('./routes/bookingRoutes');
 
 const app = express();
 
-app.set('trust proxy', true);
+app.set('trust proxy', 1);
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
